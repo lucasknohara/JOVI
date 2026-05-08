b43bdfb57f765c8e03a31a6cb8baeba9d84d5c5b
Laboratório FIAP <labsfiap@fiap.com.br>
2026-05-08 11:39:43 -0300

    commit teste

---
+++ src/pageConfigModo/script.js
@@ -0,0 +1,26 @@
+const botao = document.querySelector("#btnCriar");
+
+botao.addEventListener("click", () => {
+    const nomeModo = document.querySelector("#campoNome").value;
+    const iconeSelecionado = document.querySelector('input[name="selecao-icone"]:checked')
+
+    if (nomeModo.trim() === "") {
+        alert("Nome Obrigatorio!");
+        return;
+    };
+
+    const novoModo = {
+        nome: nomeModo,
+        icone: iconeSelecionado.value,
+    };
+
+    let modos = JSON.parse(localStorage.getItem("modos")) || [];
+
+    modos.push(novoModo)
+
+    localStorage.setItem("modos", JSON.stringify(modos));
+
+    alert("Modo Criado!");
+
+    window.location.href = "../pageMeusModos/index.html";
+});
